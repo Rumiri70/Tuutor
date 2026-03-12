423105c376a286aeb4bf92008c174395d189daff
atthejacksons <atthejacksons@example.com>
2026-03-12 17:40:19 +0300

    featured video

--- includes/class-tuutor-display.php
+++ includes/class-tuutor-display.php
@@ -49,6 +49,14 @@ class Tuutor_Display
                         <?php the_post_thumbnail('large'); ?>
                     </div>
                 <?php endif; ?>
+                
+                <?php 
+                $featured_video = do_shortcode('[tuutor_featured_video]');
+                if (!empty($featured_video)) {
+                    echo $featured_video;
+                }
+                ?>
+
                 <h1>
                     <?php the_title(); ?>
                 </h1>
